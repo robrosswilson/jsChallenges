f1a2ae70a7ac69bd115c3d7592ca908ac73d1e04
Rob Wilson <robertrosswilson98@gmail.com>
2022-12-12 14:21:02 +0000

    firstcommit

---
+++ test.js
@@ -0,0 +1,13 @@
+// xxxx
+
+console.log("hello".length)
+console.log("Hello, what is your name?".length)
+console.log("Hello".toUpperCase())
+
+console.log("      hello      ".trim())
+
+let num = 0.001658853;
+num.toPrecision(2);
+num.toPrecision(3);
+num.toPrecision(10);
+print(num)
